dbe56d43534c0459226a417a733f38a64c6bde19
Lindsey Dietz <diet0146@umn.edu>
2020-12-12 09:44:48 -0600

    renaming most things

---
+++ doc/BSA_Slides.R
@@ -0,0 +1,105 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  echo = TRUE,
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----ldck, echo=FALSE, out.width = '40%', fig.show='hold', fig.align='center'----
+knitr::include_graphics("pics/boston.jpg")
+knitr::include_graphics("pics/ldck.jpg")
+
+## ----setup, message=FALSE, eval=FALSE-----------------------------------------
+#  library(MyFirstPackage)
+#  
+#  # Design your R package
+#  design_package()
+#  
+#  # Build your R package
+#  build_package()
+#  
+#  # Distribute your R package
+#  distribute_package()
+#  
+#  #Profit!
+
+## ----pam, echo=FALSE, out.width = '100%'--------------------------------------
+knitr::include_graphics("pics/pam.jpeg")
+
+## ---- echo=FALSE, out.width = '90%', fig.align='center'-----------------------
+knitr::include_graphics("pics/Map.jpg")
+
+## ---- echo=FALSE, out.width = '100%', fig.align='center'----------------------
+knitr::include_graphics("pics/twoseparate.jpg")
+
+## ---- echo=FALSE, out.width = '100%', fig.align='center'----------------------
+knitr::include_graphics("pics/FoldItIn.jpg")
+
+## ---- echo=FALSE, out.width = '50%', fig.align='center'-----------------------
+knitr::include_graphics("pics/LadleStir.jpg")
+
+## ---- echo=FALSE, out.width = '60%', fig.align='center'-----------------------
+knitr::include_graphics("pics/MothersRecipe.jpg")
+
+## ---- echo=FALSE, out.width = '100%'------------------------------------------
+knitr::include_graphics("pics/BurningSmellsLike.jpg")
+
+## ----glmm, echo=FALSE, out.width = '60%', fig.align='center'------------------
+knitr::include_graphics("pics/glmm.png")
+
+## ----lego1, echo=FALSE, out.width = '100%'------------------------------------
+knitr::include_graphics("pics/lego.jpeg")
+
+## ----setup3, eval=FALSE, message=FALSE----------------------------------------
+#  #install.packages('usethis')
+#  library(usethis)
+#  
+#  usethis::create_package("~/MyFirstPackage")
+#  
+
+## ----setup4, eval=FALSE, message=FALSE----------------------------------------
+#  
+#  usethis::use_r('target_psrf')
+#  usethis::use_r('minESS')
+#  
+
+## ----miness, echo=FALSE, out.width = '100%'-----------------------------------
+knitr::include_graphics("pics/partialfunc.jpeg")
+
+## ----miness2, echo=FALSE, out.width = '100%'----------------------------------
+knitr::include_graphics("pics/fullfunc.jpeg")
+
+## ----doc, echo=FALSE, out.width = '75%'---------------------------------------
+knitr::include_graphics("pics/doc.jpg")
+
+## ----build, echo=FALSE, out.width = '80%'-------------------------------------
+knitr::include_graphics("pics/check.jpg")
+
+## ----check, eval = FALSE, echo=TRUE-------------------------------------------
+#  devtools::check()
+#  devtools::build()
+#  devtools::install()
+
+## ----bedazzle, echo=FALSE, out.width = '100%'---------------------------------
+knitr::include_graphics("pics/bedazzler.jpg")
+
+## ----janet, echo=FALSE, out.width = '100%'------------------------------------
+knitr::include_graphics("pics/janets.png")
+
+## ----dr, echo=FALSE, out.width = '90%'----------------------------------------
+knitr::include_graphics("pics/dr.jpeg")
+
+## ----organized, echo=FALSE, out.width = '100%'--------------------------------
+knitr::include_graphics("pics/ann.jpg")
+
+## ----git, echo=FALSE, out.width = '49%'---------------------------------------
+knitr::include_graphics("pics/github.jpg")
+knitr::include_graphics("pics/createrepo.jpg")
+
+
+## ----gitpush, echo=FALSE, out.width = '70%', fig.align='center'---------------
+knitr::include_graphics("pics/gitpush.jpg")
+
+## ----incepted, echo=FALSE, out.width = '100%'---------------------------------
+knitr::include_graphics("pics/inception.jpg")
+
